8318861b6e58e5b8725688fe296a5e4f135efbc4
gordun209 <ali-han80@hotmail.com>
2022-07-24 16:27:46 +0300

    rtk query boilerplate

---
+++ src/features/auth/authSlice.ts
@@ -0,0 +1,31 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+import type { AppState } from '@/app/store';
+
+type authStateType = {
+  user: {
+    id: number;
+    username: string;
+    email: string;
+    company?: string;
+  } | null;
+};
+
+const initialState: authStateType = {
+  user: null,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    setAuth: (state, action) => {
+      state.user = action.payload;
+    },
+  },
+});
+
+export const { setAuth } = authSlice.actions;
+export default authSlice.reducer;
+
+export const selectUser = (state: AppState) => state.auth.user;
